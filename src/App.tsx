d169cf93841e5cc5107c6c62e6cb4d647db2e334
Pedro Ferreira <pedorferreira@ferreirasolucoes.site>
2021-05-14 19:04:20 -0300

    add Reacr-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+import Modal from "react-modal";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement("#root");
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenTransactionNewModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+  function handleCloseTransactionNewModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenTransactionNewModal} />
       <Dashboard />
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onAfterOpen={handleOpenTransactionNewModal}
+        onRequestClose={handleCloseTransactionNewModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
     </>
   );
 }
